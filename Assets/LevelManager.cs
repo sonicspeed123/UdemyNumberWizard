f495730e69eb1047f2dc8b5591a7d40a78c02d22
sonicspeed123 <joshuasahagun1996@yahoo.com>
2017-08-16 13:49:15 -0700

    Initial commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Needed to change scenes/levels
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		//takes name and loads level based on name
+		SceneManager.LoadScene (name);
+	}
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+	int maxGuesses = 10;
+
+	public Text text;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		NextGuess ();
+
+	}
+
+	void NextGuess () {
+		guess = Random.Range(min, max + 1);
+		maxGuesses--;
+		text.text = guess.ToString();
+		if (maxGuesses <= 0) {
+			SceneManager.LoadScene ("Win");
+		}
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+		
+}
